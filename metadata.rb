cdc4e541969130cf992ae25c2f03b839db09293c
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2013-06-03 16:49:01 +0100

    Install Vagrant

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,11 @@
 name             'vagrant'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
+maintainer       'Protec Innovations'
+maintainer_email 'dev@protecinnovations.co.uk'
+license          'BSD'
 description      'Installs/Configures vagrant'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+%w{ php }.each do |cb|
+  depends cb
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,18 @@
 # Cookbook Name:: vagrant
 # Recipe:: default
 #
-# Copyright 2013, YOUR_COMPANY_NAME
+# Copyright 2013, Protec Innovations
 #
-# All rights reserved - Do Not Redistribute
+# Licenced Under BSD Licence
 #
+
+remote_file "/tmp/vagrant_1.2.2_x86_64.deb" do
+  source "http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/vagrant_1.2.2_x86_64.deb"
+  mode 0644
+  checksum "d3035d259a28bd7090ef00629e61cc0520778143fad06f49adb3c45a75fb1425"
+end
+
+dpkg_package "vagrant" do
+  source "/tmp/vagrant_1.2.2_x86_64.deb"
+  action :install
+end
