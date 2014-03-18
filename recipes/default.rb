bbedbc48373fe8c7f554f7747f48eb0d897eda2e
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2014-03-18 11:26:48 +0000

    BugzID: 599 - Update Vagrant

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,13 +7,13 @@
 # Licenced Under BSD Licence
 #
 
-remote_file "/tmp/vagrant_1.2.2_x86_64.deb" do
-  source "http://files.vagrantup.com/packages/7e400d00a3c5a0fdf2809c8b5001a035415a607b/vagrant_1.2.2_x86_64.deb"
+remote_file "/tmp/vagrant_1.5.1_x86_64.deb" do
+  source "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.1_x86_64.deb"
   mode 0644
-  checksum "d3035d259a28bd7090ef00629e61cc0520778143fad06f49adb3c45a75fb1425"
+  checksum "47c25bfcbb44199c2a9c61c9478510b734ff950e64c80e9ffbe382f6f702cea3"
 end
 
 dpkg_package "vagrant" do
-  source "/tmp/vagrant_1.2.2_x86_64.deb"
+  source "/tmp/vagrant_1.5.1_x86_64.deb"
   action :install
 end
