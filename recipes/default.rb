2939856719e6d00577638d0f11c938d29b2c83c5
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2015-06-04 11:46:20 +0100

    Update to latest version

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,13 +7,13 @@
 # Licenced Under BSD Licence
 #
 
-remote_file "/tmp/vagrant_1.5.3_x86_64.deb" do
-  source "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.5.3_x86_64.deb"
+remote_file "/tmp/vagrant_1.7.2_x86_64.deb" do
+  source "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb"
   mode 0644
-  checksum "430c5553aeb3f2f5ff30c8c32a565db16669eaf76a553e3e1ceff27cbe6cb2b2"
+  checksum "9d7f1c587134011e2d5429eb21b6c0e95487f52e6d6d47c03ecc82cbeee73968"
 end
 
 dpkg_package "vagrant" do
-  source "/tmp/vagrant_1.5.3_x86_64.deb"
+  source "/tmp/vagrant_1.7.2_x86_64.deb"
   action :install
 end
