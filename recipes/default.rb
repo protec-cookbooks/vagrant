cf1f26a8cbe4010a8606d21ac7e16c853621c0fc
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2015-07-13 14:04:14 +0100

    Upgrade Vagrant

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,13 +7,13 @@
 # Licenced Under BSD Licence
 #
 
-remote_file "/tmp/vagrant_1.7.2_x86_64.deb" do
-  source "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2_x86_64.deb"
+remote_file "/tmp/vagrant_1.7.3_x86_64.deb" do
+  source "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.3_x86_64.deb"
   mode 0644
-  checksum "9d7f1c587134011e2d5429eb21b6c0e95487f52e6d6d47c03ecc82cbeee73968"
+  checksum "755ab4a3e4e076c96cb260bf67c7f08b8186a2340d7dc2f1df98d7fc7ad3e27d"
 end
 
 dpkg_package "vagrant" do
-  source "/tmp/vagrant_1.7.2_x86_64.deb"
-  action :install
+  source "/tmp/vagrant_1.7.3_x86_64.deb"
+  action :upgrade
 end
