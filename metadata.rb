554bef3b89abdfd086e50569e04ac54b5eea4a95
Martin Meredith <martinmeredith@protecinnovations.co.uk>
2015-09-02 13:12:55 +0100

    New Upstream version

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,4 @@ maintainer_email 'dev@protecinnovations.co.uk'
 license          'BSD'
 description      'Installs/Configures vagrant'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
-
-%w{ php }.each do |cb|
-  depends cb
-end
+version          '0.1.2'
--- recipes/default.rb
+++ recipes/default.rb
@@ -7,13 +7,13 @@
 # Licenced Under BSD Licence
 #
 
-remote_file "/tmp/vagrant_1.7.3_x86_64.deb" do
-  source "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.3_x86_64.deb"
+remote_file "/tmp/vagrant_1.7.4_x86_64.deb" do
+  source "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4_x86_64.deb"
   mode 0644
-  checksum "755ab4a3e4e076c96cb260bf67c7f08b8186a2340d7dc2f1df98d7fc7ad3e27d"
+  checksum "dcd2c2b5d7ae2183d82b8b363979901474ba8d2006410576ada89d7fa7668336"
 end
 
 dpkg_package "vagrant" do
-  source "/tmp/vagrant_1.7.3_x86_64.deb"
+  source "/tmp/vagrant_1.7.4_x86_64.deb"
   action :upgrade
 end
